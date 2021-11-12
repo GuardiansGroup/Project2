5f4f7dc2ef9cdcdfff351d4f912ec3cc26d0f151
Daniel Perez <89610746+danielperez19@users.noreply.github.com>
2021-11-11 21:14:16 -0800

    Fixed Changes for the LinkedTest class testing for it.

--- LinkedStackedTest.java
+++ LinkedStackedTest.java
@@ -1,8 +1,9 @@
 public class LinkedStackedTest {
-    public static void main(String[] args) {
-        var linkedtest = new LinkedStack<Integer>(); 
-        String expression = "a*b/(c-a)+d*e"; 
-        int convertToPostfix = linkedtest.convertToPostfix(expression);  
-        System.out.println(convertToPostfix); 
-    }
+   public static void main(String[] args) 
+   {
+      String expression = "a*b/(c-a)+d*e";
+      var linkedtest = new LinkedStack<Integer>(); 
+      String output = LinkedStack.convertToPostfix(expression);
+      System.out.println(output);
+   }
 }
